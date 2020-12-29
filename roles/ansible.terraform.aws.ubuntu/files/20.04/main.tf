e3273efa1aead37d54ee407d3e337bf04dc625ce
Sirtoli Sébastien <sirtoli.dev@gmail.com>
2020-12-29 17:28:36 +0100

    Add port 9100 and comment for avoid error

--- roles/ansible.terraform.aws.ubuntu/files/20.04/main.tf
+++ roles/ansible.terraform.aws.ubuntu/files/20.04/main.tf
@@ -35,6 +35,12 @@ provider "aws" {
 //    protocol    = "tcp"
 //    cidr_blocks = ["0.0.0.0/0"]
 //  }
+//    ingress {
+//    from_port   = 9100
+//    to_port     = 9100
+//    protocol    = "tcp"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
 //   egress {
 //    from_port   = 0
 //    to_port     = 0
