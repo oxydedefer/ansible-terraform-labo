3024d89cf643978a859c909b2515b2fc3336d267
Sirtoli Sébastien <sirtoli.dev@gmail.com>
2020-12-29 12:16:35 +0100

    Comment for avoid error duplicate security group and key pair

--- roles/ansible.terraform.aws.ubuntu/files/20.04/main.tf
+++ roles/ansible.terraform.aws.ubuntu/files/20.04/main.tf
@@ -9,45 +9,46 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = var.ssh_public_key
-}
-
-resource "aws_security_group" "labo" {
-  name = "generate-security-terraform"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+//resource "aws_key_pair" "deployer" {
+//  key_name   = "deployer-key"
+//  public_key = var.ssh_public_key
+//}
+//
+//resource "aws_security_group" "labo" {
+//  name = "generate-security-terraform"
+//
+//  ingress {
+//    from_port   = 22
+//    to_port     = 22
+//    protocol    = "tcp"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//  ingress {
+//    from_port   = 80
+//    to_port     = 80
+//    protocol    = "tcp"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//    ingress {
+//    from_port   = 8080
+//    to_port     = 8080
+//    protocol    = "tcp"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//   egress {
+//    from_port   = 0
+//    to_port     = 0
+//    protocol    = "-1"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//}
 
 resource "aws_instance" "labo" {
   ami           = "ami-0aef57767f5404a3c"
   instance_type = "t2.micro"
   key_name      = "deployer-key"
-  vpc_security_group_ids = [aws_security_group.labo.id]
+//  vpc_security_group_ids = [aws_security_group.labo.id]
+  vpc_security_group_ids = ['sg-0095033037ad91574']
   tags = {
     Name = "Labo-ubuntu-${var.vm_version} "
   }
